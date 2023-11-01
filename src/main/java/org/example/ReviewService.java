6501be3f5e063c6eab62fff81c95c81a2225aeb4
ALMA_DEV914 <aungonalna58@gamil.com>
2023-11-01 13:17:56 -0700

    add controller and service class

--- src/main/java/org/example/ReviewService.java
+++ src/main/java/org/example/ReviewService.java
@@ -2,10 +2,11 @@ package org.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.ReactiveUpdateOperation;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ReviewService {
     @Autowired
     private ReviewRepository reviewRepository;
@@ -13,12 +14,13 @@ public class ReviewService {
     private MongoTemplate mongoTemplate;
 
     public Review createReview(String reviewBody, String imdbId) {
+
         Review review = new Review(reviewBody);
         reviewRepository.insert(review);
 
         mongoTemplate.update(Movie.class)
                 .matching(Criteria.where("imdbId").is(imdbId))
-                .apply(new Update().push("reviewsIds").value(review))
+                .apply(new Update().push("reviewIds").value(review))
                 .first();
 
         return review;
